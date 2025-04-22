5566464b235b2cfb2baf5dfa1d6d7e354cb55427
sank-45 <sank4patil@gmail.com>
2025-04-22 15:10:13 +0530

    scss error solve

--- src/Portfolio/Component/Navbar/NavBar.jsx
+++ src/Portfolio/Component/Navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import './Navbar.scss';
+import './NavBar.scss';
 import Logo from "../../Component/Assets/logo1.png";
 import { FaPhoneAlt } from 'react-icons/fa';
 import { IoMenuSharp } from "react-icons/io5";
