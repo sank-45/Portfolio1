a7212739eaf1ffe946566f53c8a381fe2644a70e
sank-45 <sank4patil@gmail.com>
2025-04-24 13:31:26 +0530

    responsiveness

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
-    modules:{
-    localsConvention:"camelCase",
+  server: {
+    host: '0.0.0.0', // 👈 this makes Vite listen to all IPs
+    port: 5173,      // 👈 or any other port you want
   },
-},
-})
+});
